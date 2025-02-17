45ffedf9ab301413b93f3b63fa5e7a339d716d3f
Brett Smith <brett03112@gmail.com>
2025-02-17 16:15:21 -0500

    Updated project structure

--- SimpleIO/SimpleIO.Tests/obj/Debug/net9.0/SimpleIO.Tests.AssemblyInfo.cs
+++ SimpleIO/SimpleIO.Tests/obj/Debug/net9.0/SimpleIO.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleIO.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3eb1d8aac80e265372a7ca18a09a603f6ac4adb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bb1677a609b3e7a736bb938629291d982c7f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleIO.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleIO.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
